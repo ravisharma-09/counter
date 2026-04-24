331d0dccb0e51d014ad7025f259ee1af042701a0
raviisharma09 <2102508788@svyasa-sas.edu.in>
2026-04-24 12:19:24 +0530

    ui done logic pending

---
+++ index.js
@@ -0,0 +1,6 @@
+const plus = document.querySelector("#plus") ;
+const minus = document.querySelector("#minus") ;
+const reset = document.querySelector("#reset") ;
+const two = document.querySelector("#twomultiply") ;
+const bytwo = document.querySelector("#bytwo") ;
+const three = document.querySelector("#threemultiply") ;
