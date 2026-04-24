36099f68539c6c83c4260d21377ee94c32b6ceae
raviisharma09 <2102508788@svyasa-sas.edu.in>
2026-04-24 12:38:00 +0530

    done

--- index.js
+++ index.js
@@ -4,3 +4,29 @@ const reset = document.querySelector("#reset") ;
 const two = document.querySelector("#twomultiply") ;
 const bytwo = document.querySelector("#bytwo") ;
 const three = document.querySelector("#threemultiply") ;
+const display = document.querySelector("#counter") ;
+let a = 0 ;
+plus.addEventListener("click",() =>{
+    a++
+    display.innerText = a ;
+})
+minus.addEventListener("click",()=>{
+    a--
+    display.innerText = a ;
+})
+two.addEventListener("click",() =>{
+    a =a*2
+    display.innerText = a ;
+})
+bytwo.addEventListener("click",()=>{
+    a =a/2
+    display.innerText = a ;
+})
+three.addEventListener("click",() =>{
+    a = a*3
+    display.innerText = a ;
+})
+reset.addEventListener("click",()=>{
+    a = 0
+    display.innerText = a;
+})
